db06b96e67f471dd7f2293a0764ba06ec2dfcf9c
20Federico <fefe.molino@gmail.com>
2021-11-09 16:05:05 +0100

    initial structure

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict'
+Vue.config.devtools = true;
+
+// Descrizione:
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+window.addEventListener('DOMContentLoaded', function () {
+  new Vue({
+      el: '',
+      data: {
+        username: '',
+      },
+      methods: {
+        function () {
+          
+        },
+      },
+  });
+})
